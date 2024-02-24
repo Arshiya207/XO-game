cb20dfe59a252daef7cfa31490025d6affd4629d
Arshiya207 <arshiya1383.mk@gmail.com>
2024-02-24 16:04:09 +0330

    add animations to css ready to use

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const cellsArr = document.querySelectorAll(".cell");
+const firstPersonName = document.querySelector("#firstP").value;
+const secondPersonName = document.querySelector("#secondP").value;
+const firstPersonChar = document.querySelector("#firstChar").value;
+const secondPersonChar = document.querySelector("#secondChar").value;
+let trun = 1;
+
+cellsArr.forEach((cell) => {
+  cell.addEventListener("click", (e) => {});
+});
