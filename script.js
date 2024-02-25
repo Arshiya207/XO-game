5abb1c545bbcfc9c66a587fda11510afa7a2fc5a
Arshiya207 <arshiya1383.mk@gmail.com>
2024-02-25 16:09:57 +0330

    finish writing win function

--- script.js
+++ script.js
@@ -11,6 +11,16 @@ const resetBtn = document.querySelector(".reset-btn");
 let turn = 1;
 let numberOfClicksForStartBtn = 1;
 let isGameStart = false;
+const winArray = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 //* functions
 //* cell function
 function cellFunction(e) {
@@ -18,10 +28,22 @@ function cellFunction(e) {
   if (turn === 1) {
     this.classList.add(firstPersonChar);
     infoText.innerHTML = "it's " + secondPersonName + "'s turn";
+    if (evaluateFunction(turn)) {
+      alert("firstPerson won");
+      isGameStart = false;
+      infoText.innerHTML = firstPersonName + " won the game";
+      return;
+    }
     turn = 2;
   } else if (turn === 2) {
     this.classList.add(secondPersonChar);
     infoText.innerHTML = "it's " + firstPersonName + "'s turn";
+    if (evaluateFunction(turn)) {
+      alert("secondPerson won");
+      isGameStart = false;
+      infoText.innerHTML = secondPersonName + " won the game";
+      return;
+    }
     turn = 1;
   }
 }
@@ -49,11 +71,47 @@ function resetFunction(e) {
   cellsArr.forEach((cell) => {
     cell.classList.remove(firstPersonChar);
     cell.classList.remove(secondPersonChar);
+    cell.querySelector(".overlay").classList.remove("green");
   });
 
   alert("board is now clear. click on start");
 }
+//-----
+function evaluateFunction(turn) {
+  if (turn === 1) {
+    const finalResult = winArray.some((winArr) => {
+      const semiResult = winArr.every((indexes) => {
+        return cellsArr[indexes].classList.contains(firstPersonChar);
+      });
 
+      if (semiResult) {
+        winArr.forEach((cellIndex) => {
+          cellsArr[cellIndex].querySelector(".overlay").classList.add("green");
+        });
+        return semiResult;
+      }
+      return semiResult;
+    });
+
+    return finalResult;
+  } else if (turn === 2) {
+    const finalResult = winArray.some((winArr) => {
+      const semiResult = winArr.every((indexes) => {
+        return cellsArr[indexes].classList.contains(secondPersonChar);
+      });
+
+      if (semiResult) {
+        winArr.forEach((cellIndex) => {
+          cellsArr[cellIndex].querySelector(".overlay").classList.add("green");
+        });
+        return semiResult;
+      }
+      return semiResult;
+    });
+
+    return finalResult;
+  }
+}
 //* end functions
 //! start btn code
 startBtn.addEventListener("click", startFunction);
